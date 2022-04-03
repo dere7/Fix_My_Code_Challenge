947400b2c87599a67944f63640784275e9ea11f0
Dereje Desta <ddesta777@gmail.com>
2022-04-03 23:34:40 +0300

    worked 0x01-challenge 0 to 2

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Contains a Sqaure"""
+
+
+class Square():
+    """It is Rectangle not Square. :)"""
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the Square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter of the Square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
